54151e23b5c3d714c06fdd38c4fda235064953bd
miguelmendes <miguel.lalex.mendes@gmail.com>
2021-02-23 20:30:47 +0000

    implementacao de alguns metodos dos elevadores #2

--- src/Elevator.java
+++ src/Elevator.java
@@ -1,8 +1,13 @@
+import static java.lang.Thread.sleep;
+
 public class Elevator {
 
   private final int floorMin;
   private final int floorMax;
   public int curFloor = 0;
+  boolean emergency = false;
+  boolean pasWaiting = false;
+  boolean doorsOpen = false;
 
   public Elevator(int floorMin, int floorMax, int curFloor){
     this.curFloor = curFloor;
@@ -10,21 +15,73 @@ public class Elevator {
     this.floorMin = floorMin;
 
   }
-//falta fazer a contagem dos segundos
-  public int call(int num){
-    int count=0;
-    if(num>floorMin || num>floorMax){
-      mover(num);
-      return count;
+
+  public int call(int num) throws InterruptedException {
+    if(num>=floorMin && num<=floorMax){
+      int temp = mover(num);
+      System.out.println("open doors");
+      doorsOpen = true;
+      return temp;
+
     }
     else{
       System.out.println("andar nao disponivel");
-      return count;
+      return 0;
     }
   }
 
-  public int mover(int num){
+  public int travelTo(int num) throws InterruptedException{
+
+    if(num>=floorMin && num<=floorMax){
+      System.out.println("close doors");
+      doorsOpen = false;
+      int temp = mover(num);
+      System.out.println("open doors");
+      doorsOpen = true;
+      return temp;
+
+    }
+    else{
+      System.out.println("andar nao disponivel");
+      return 0;
+    }
 
   }
+
+  public int mover(int num) throws InterruptedException {
+    int mov = Math.abs(num - curFloor);
+    boolean upElevator;
+    int temp = 0;
+
+    upElevator = num >= curFloor;
+
+    for(int i = 0; i<mov; i++){
+      sleep1Sec();
+      temp++;
+      if (upElevator){
+        curFloor ++;
+      }
+      else{
+        curFloor --;
+      }
+      System.out.println("floor " + curFloor);
+    }
+
+    return temp;
+
+  }
+
+  public void sleep1Sec() throws InterruptedException {
+    sleep(1000);
+  }
+
+
+  public static void main(String[] args) throws InterruptedException {
+    Elevator E1 = new Elevator(-1,9,1);
+    System.out.println(E1.call(9));
+
+
+  }
+
 
 }
